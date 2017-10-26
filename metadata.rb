7e4c793a60849b9ae2903c73a76348824de8db8b
ghariosk <karlgharios@hotmail.com>
2017-10-26 17:41:34 +0100

    fixed version

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name 'mongodb-server'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures mongodb--server'
+description 'Installs/Configures mongodb-server'
 long_description 'Installs/Configures mongodb--server'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -18,3 +18,7 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/mongodb--server'
+
+
+# berks update
+
